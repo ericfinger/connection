2e3c66f3f17cea4b26b4e967db8a8ad1af5cf1fc
Eric Finger <fingereric@web.de>
2022-11-28 19:28:56 +0100

    first working prototype
    (works with my knockd setup)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::net::SocketAddr;
+use socket2::{Socket, Domain, Type};
+use dns_lookup::getaddrinfo;
+
 fn main() {
-    println!("Hello, world!");
+    let ports = [1234, 5678, 910, 1112, 1314];
+    let host = "example.com";
+    let gai = getaddrinfo(Some(host), None, None).unwrap();
+    let gai_result = gai.collect::<std::io::Result<Vec<_>>>().unwrap();
+    let sa = &gai_result[0].sockaddr;
+    let ip = sa.ip();
+
+    for port in ports {
+        let socket = if ip.is_ipv4() {
+             Socket::new(Domain::IPV4, Type::STREAM, None).unwrap()
+        } else {
+            Socket::new(Domain::IPV6, Type::STREAM, None).unwrap()
+        };
+        socket.set_nonblocking(true).unwrap();
+
+        let address = SocketAddr::new(ip, port);
+        let address = address.into();
+
+        // let _ = socket.connect(&address);
+        println!("Connecting to {}:{}", ip, port);
+        match socket.connect(&address) {
+            Ok(_) => {},
+            Err(_) => {
+                // We expect this to fail, the port is probably closed after all
+            }
+        }
+
+        drop(socket);
+        std::thread::sleep(std::time::Duration::from_micros(1000 * 100));
+    }
 }
+
